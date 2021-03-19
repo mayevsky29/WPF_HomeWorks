c2d05d07024ec2382de123c9e6ca8dc4b1074e68
mayevskyivan5@gmail.com <mayevskyivan5@gmail.com>
2021-03-19 17:38:01 +0200

    handling exceptions

---
+++ ExceptionsWPF/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ExceptionsWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_DispatcherUnhandledException(object sender, 
+            System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        // DispatcherUnhandledException дозволяє обробляє необроболені винятки
+        // DispatcherUnhandledException="Application_DispatcherUnhandledException потрібно додати як ресурс в app.xampl
+
+        // присвоєння властивості e.Handled значення true вказує про завершення роботи над необробленим винятком
+        {
+            MessageBox.Show("Щойно стався необроблений виняток: " 
+                + e.Exception.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+            e.Handled = true;
+        }
+        // Таким чином, в програмуванні бажано використовувати обробку винятків глобально
+        // оскільки локальна обробка більш деталізована, що дозволяє вирішувати проблему іншим способом
+    }
+}
---
+++ ExceptionsWPF/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExceptionsWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //string s = null;
+            //// якщо змінна є null, то це виклакає помилку 
+            //s.Trim();
+
+            // В таких випадках потрібно використовувати try-catch
+            // властивість Warning деталізує, в чому полягає помилка
+            string s = null;
+            try
+            {
+                s.Trim();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Щойно відбулася обробка винятку: " 
+                    + ex.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            s.Trim();
+        }
+    }
+}
